a2279b5febd6a710be34ae89b1ab929ad315886e
raffaele-catalano <raf.catalano@gmail.com>
2023-06-07 17:07:27 +0200

    added content dinamically to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>test</h1>
+        <div class="container py-3">
+            <h1 class="py-2">{{ $title }}</h1>
+            <p>
+                {{ $text }}
+            </p>
+            <a href="{{ route('movies') }}" class="btn btn-warning fw-bold">Go To Movies List</a>
+        </div>
 @endsection
