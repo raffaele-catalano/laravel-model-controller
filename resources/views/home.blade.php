58dd853a5ea0129baf9c4f12cd92908b7fa0dd25
raffaele-catalano <raf.catalano@gmail.com>
2023-06-07 18:58:39 +0200

    minor fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
         <div class="container py-3">
-            <h1 class="py-2">{{ $title }}</h1>
-            <p>
+            <div class="logo-container mb-3">
+                <img src="/moviedb_logo.png" alt="logo">
+            </div>
+            <h1 class="text-center py-2">{{ $title }}</h1>
+            <p class="mb-3">
                 {{ $text }}
             </p>
-            <a href="{{ route('movies') }}" class="btn btn-warning fw-bold">Go To Movies List</a>
+            <a href="{{ route('movies') }}" class="btn btn-movies btn-warning w-25 fw-bold">Go To Movies List</a>
         </div>
 @endsection
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
     <div class="movies-container">
-        @foreach ($movies as $movie )
-            <h2 class="text-warning">Titolo: {{ $movie->title }}</h2>
-        @endforeach
-        <a href="{{ route('home') }}" class="btn btn-warning fw-bold">Home</a>
+        <h2 class="text-warning text-center fw-bold">Movies</h2>
+        <div class="row row-cols-5 gy-2 mb-3">
+            @foreach ($movies as $movie)
+                <div class="card bg-dark text-light p-3">
+                    <img src="{{ $movie->img_path }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-title">Titolo Originale: {{ $movie->original_title }}</h6>
+                        <p class="card-title">Nazionalità: {{ $movie->nationality }}</p>
+                        <p class="card-title">Data di Uscita: {{ $movie->date }}</p>
+                        <p class="card-title">Voto: {{ $movie->vote }}</p>
+                    </div>
+                    <a href="#" class="btn btn-details btn-warning w-50">Dettagli</a>
+                </div>
+            @endforeach
+        </div>
+        <a href="{{ route('home') }}" class="btn btn-home btn-danger fw-bold my-3">Home</a>
     </div>
 @endsection
